d04233c2b167b059df6c48a4b19ae384d4802c47
hiencoder <nguyenhien1989hd@gmail.com>
2018-04-10 22:28:05 +0700

    Init project

---
+++ app/src/main/java/vn/edu/imic/typeobservable/observable/ObservableActivity.java
@@ -0,0 +1,15 @@
+package vn.edu.imic.typeobservable.observable;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import vn.edu.imic.typeobservable.R;
+
+public class ObservableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
